563c6117cfd6800f9aec63f0cda0a2d141ea26cd
Shin-kyoto <aquashin0202@gmail.com>
2024-07-10 20:48:55 +0900

    feat: add comparison code
    Signed-off-by: Shin-kyoto <aquashin0202@gmail.com>

--- pcd_type_updater/constant.py
+++ pcd_type_updater/constant.py
@@ -1,3 +1,4 @@
-current_webauto_version: list = [
+current_webauto_versions: list = [
     "refs/tags/v0.34.0",
+    "refs/tags/v0.35.0",
 ]
--- pcd_type_updater/pcd_type_updater.py
+++ pcd_type_updater/pcd_type_updater.py
@@ -4,8 +4,17 @@ from pathlib import Path
 import yaml
 import subprocess
 from convert_pointcloud_types import process_bag, get_rosbag_options
-from constant import current_webauto_version
+from constant import current_webauto_versions
 import rosbag2_py
+
+# compare
+from rclpy.serialization import deserialize_message
+from rosidl_runtime_py.utilities import get_message
+from collections import defaultdict
+from typing import NamedTuple
+from sensor_msgs.msg import PointCloud2
+import numpy as np
+import ros2_numpy
 
 
 class WebAutoT4DatasetInterface:
@@ -21,11 +30,10 @@ class WebAutoT4DatasetInterface:
             .split(" ")[-1]
         )
 
-        self.current_webauto_version: list = current_webauto_version
-        if self.webauto_version not in self.current_webauto_version:
+        if self.webauto_version not in current_webauto_versions:
             raise ValueError(
                 f"Web.Auto version {self.webauto_version} is not supported. "
-                f"Please use one of the following versions: {self.current_webauto_version}"
+                f"Please use one of the following versions: {current_webauto_versions}"
             )
 
     def _extract_dataset_path_from_webauto_pull_output(self, output_str: str) -> str:
@@ -69,18 +77,18 @@ class WebAutoT4DatasetInterface:
         """
 
         t4dataset_path_original: Path = self._download_dataset(t4dataset_id)
-        dataset_version: str = t4dataset_path_original.name
+        dataset_id: str = t4dataset_path_original.parent.name
         rosbag_name: str = sorted(
             sorted(t4dataset_path_original.glob("input_bag"))[0].glob("*.db3")
         )[0].stem
         # 右端から，_0を削除
         rosbag_name = rosbag_name[: rosbag_name.rfind("_")]
-        move_cmd = f"mv {t4dataset_path_original} {self.work_dir_path}"
+        move_cmd = f"mv {t4dataset_path_original} {self.work_dir_path / dataset_id}"
         subprocess.run(move_cmd, shell=True)
 
-        return dataset_version, rosbag_name
+        return dataset_id, rosbag_name
 
-    def push(self, t4dataset_path: Path):
+    def push(self, t4dataset_path: Path, t4dataset_id: str):
         """
         Upload rosbag to Web.Auto
         Args:
@@ -88,69 +96,237 @@ class WebAutoT4DatasetInterface:
         """
         print(f"Uploading t4dataset: {t4dataset_path}")
 
-        raise NotImplementedError("Not implemented yet")
         upload_cmd = (
-            f"webauto data annotation-dataset push "
+            f"webauto data annotation-dataset push-version"
+            f"--annotation-dataset-id {t4dataset_id}"
             f"--project-id {self.project_id} "
-            f"--annotation-dataset-path {t4dataset_path}"
+            f"--path {t4dataset_path}"
         )
         subprocess.run(upload_cmd, shell=True)
 
 
-def get_topic_nums(bag_path: Path):
-    input_bag_path = str(bag_path)
-    input_storage_options, input_converter_options = get_rosbag_options(input_bag_path)
-
-    reader = rosbag2_py.SequentialReader()
-    reader.open(input_storage_options, input_converter_options)
-
-    topic_types = reader.get_all_topics_and_types()
-
-    # Create a map for quicker lookup
-    type_map = {
-        topic_types[i].name: topic_types[i].type for i in range(len(topic_types))
-    }
-    topic_metadata = {
-        topic_types[i].name: topic_types[i] for i in range(len(topic_types))
-    }
-
-    # bag_path直下にあるyamlファイルを読み込む
-    # bag_path直下からmetadata.yamlファイルを探す
-    yaml_path = sorted(bag_path.glob("metadata.yaml"))[0]
-
-    with open(yaml_path, "r") as file:
-        rosbag_metadata_yaml = yaml.safe_load(file)
-
-    # topic_nameとmessage_countのマップを作成
-    topic_nums = {
-        topic["topic_metadata"]["name"]: topic["message_count"]
-        for topic in rosbag_metadata_yaml["rosbag2_bagfile_information"][
-            "topics_with_message_count"
-        ]
-    }
-    return type_map, topic_metadata, topic_nums
+class PointCloudData(NamedTuple):
+    msg: PointCloud2
+    timestamp: int
 
 
-def compare(t4dataset_dir_path: Path, rosbag_name_old: str, rosbag_name_new: str):
-    """
-    変換前後で
-        topic数が一緒であるべき
-        concatenated_pcdについて
-            点群数が一緒であるべき
-            xyzの値が一緒であるべき
-            intensityの値が、tolerance = hogeで一緒であるべき
-    R,Cがdummyであるべき
-    """
-    bag_path_old: Path = t4dataset_dir_path / rosbag_name_old
-    bag_path_new: Path = t4dataset_dir_path / rosbag_name_new
+class T4datasetRosbag:
+    def __init__(self, bag_path: Path):
+        self.bag_path: Path = bag_path
 
-    topic_types_old, topic_metadata_old, topic_nums_old = get_topic_nums(bag_path_old)
-    topic_types_new, topic_metadata_new, topic_nums_new = get_topic_nums(bag_path_new)
+    def read(self):
+        rosbag_reader = self._reader(self.bag_path)
+        self.topic_type_map, self.topic_metadata = self._topic_infos(rosbag_reader)
+        self.topic_nums_yaml: dict[str, int] = self._topic_nums_yaml(self.bag_path)
+        self.topic_nums: dict[str, int] = {}
+        self.pointcloud_msgs = defaultdict(list)
 
-    # topic_nums_oldとtopic_nums_newが一致するか確認
-    assert (
-        topic_nums_old == topic_nums_new
-    ), "topic_nums_old and topic_nums_new are different"
+        while rosbag_reader.has_next():
+            (topic_name, data, t) = rosbag_reader.read_next()
+
+            if topic_name not in self.topic_nums:
+                self.topic_nums[topic_name] = 1
+            else:
+                self.topic_nums[topic_name] += 1
+
+            if self.topic_type_map[topic_name] == "sensor_msgs/msg/PointCloud2":
+                msg_type = get_message(self.topic_type_map[topic_name])
+                msg = deserialize_message(data, msg_type)
+
+                self.pointcloud_msgs[topic_name].append(PointCloudData(msg, t))
+
+    @staticmethod
+    def _reader(bag_path: Path) -> rosbag2_py.SequentialReader:
+        input_bag_path = str(bag_path)
+        input_storage_options, input_converter_options = get_rosbag_options(
+            input_bag_path
+        )
+        reader = rosbag2_py.SequentialReader()
+        reader.open(input_storage_options, input_converter_options)
+
+        return reader
+
+    @staticmethod
+    def _topic_nums_yaml(bag_path: Path):
+        # bag_path直下にあるyamlファイルを読み込む
+        # bag_path直下からmetadata.yamlファイルを探す
+        yaml_path = sorted(bag_path.glob("metadata.yaml"))[0]
+
+        with open(yaml_path, "r") as file:
+            rosbag_metadata_yaml = yaml.safe_load(file)
+
+        # topic_nameとmessage_countのマップを作成
+        return {
+            topic["topic_metadata"]["name"]: topic["message_count"]
+            for topic in rosbag_metadata_yaml["rosbag2_bagfile_information"][
+                "topics_with_message_count"
+            ]
+        }
+
+    @staticmethod
+    def _topic_infos(reader: rosbag2_py.SequentialReader):
+        topic_types = reader.get_all_topics_and_types()
+
+        # Create a map for quicker lookup
+        type_map = {
+            topic_types[i].name: topic_types[i].type for i in range(len(topic_types))
+        }
+        topic_metadata = {
+            topic_types[i].name: topic_types[i] for i in range(len(topic_types))
+        }
+
+        return type_map, topic_metadata
+
+
+class ConvertedRosbagValidator:
+    def __init__(self, rosbag_path_new: str, rosbag_path_old: str):
+        self.rosbag_new = T4datasetRosbag(rosbag_path_new)
+        self.rosbag_new.read()
+        if not self.check_topic_num_consistency_with_yaml(
+            self.rosbag_new.topic_nums, self.rosbag_new.topic_nums_yaml
+        ):
+            raise ValueError("Numbers of topic between rosbag and yaml are different")
+
+        self.rosbag_old = T4datasetRosbag(rosbag_path_old)
+        self.rosbag_old.read()
+        self.check_topic_num_consistency_with_yaml(
+            self.rosbag_old.topic_nums, self.rosbag_old.topic_nums_yaml
+        )
+
+    @staticmethod
+    def check_topic_num_consistency_with_yaml(
+        topic_nums: dict, topic_nums_yaml: dict
+    ) -> bool:
+        for topic_name in topic_nums.keys():
+            if topic_name in topic_nums_yaml:
+                if topic_nums[topic_name] != topic_nums_yaml[topic_name]:
+                    print(
+                        f"Numbers of topic {topic_name} between rosbag and yaml are different"
+                    )
+                    return False
+            else:
+                if topic_nums[topic_name] != 0:
+                    print(
+                        f"Numbers of topic {topic_name} between rosbag and yaml are different"
+                    )
+                    return False
+        return True
+
+    def _compare_topic_infos(self) -> bool:
+        # topic_nums_oldとtopic_nums_newが一致するか確認
+        if not self.rosbag_new.topic_nums == self.rosbag_old.topic_nums:
+            print("Numbers of topic between old rosbag and new rosbag are different")
+            return False
+
+        # topic_types_oldとtopic_types_newが一致するか確認
+        if not self.rosbag_new.topic_type_map == self.rosbag_old.topic_type_map:
+            print("Topic types between old rosbag and new rosbag are different")
+            return False
+
+        # topic_metadata_oldとtopic_metadata_newが一致するか確認
+        for topic_name in self.rosbag_new.topic_metadata.keys():
+            if not self.rosbag_new.topic_metadata[topic_name].equals(
+                self.rosbag_old.topic_metadata[topic_name]
+            ):
+                print("Topic metadata between old rosbag and new rosbag are different")
+                return False
+
+        return True
+
+    @staticmethod
+    def _compare_pointcloud_num(
+        pointcloud_msgs_new: PointCloud2, pointcloud_msgs_old: PointCloud2
+    ) -> bool:
+        if pointcloud_msgs_new.msg.width != pointcloud_msgs_old.msg.width:
+            return False
+        return True
+
+    @staticmethod
+    def _compare_xyz_in_pointcloud(
+        pointcloud_msgs_new: PointCloud2, pointcloud_msgs_old: PointCloud2
+    ) -> bool:
+        pointcloud_array_new: np.ndarray = ros2_numpy.numpify(pointcloud_msgs_new.msg)
+        pointcloud_array_old: np.ndarray = ros2_numpy.numpify(pointcloud_msgs_old.msg)
+
+        for xyz in ["x", "y", "z"]:
+            if not np.allclose(pointcloud_array_new[xyz], pointcloud_array_old[xyz]):
+                return False
+        return True
+
+    @staticmethod
+    def _compare_intensity_in_pointcloud(
+        pointcloud_msgs_new: PointCloud2,
+        pointcloud_msgs_old: PointCloud2,
+        tolerance: float,
+    ) -> bool:
+        pointcloud_array_new: np.ndarray = ros2_numpy.numpify(pointcloud_msgs_new.msg)
+        pointcloud_array_old: np.ndarray = ros2_numpy.numpify(pointcloud_msgs_old.msg)
+
+        if not np.allclose(
+            pointcloud_array_new["intensity"],
+            pointcloud_array_old["intensity"],
+            atol=tolerance,
+        ):
+            return False
+        return True
+
+    def _compare_pointcloud_data(self) -> bool:
+        for index, pointcloud_msg_new in enumerate(
+            self.rosbag_new.pointcloud_msgs["/sensing/lidar/concatenated/pointcloud"]
+        ):
+            pointcloud_msg_old = self.rosbag_old.pointcloud_msgs[
+                "/sensing/lidar/concatenated/pointcloud"
+            ][index]
+
+            # 点群数が一緒であるべき
+            if not self._compare_pointcloud_num(pointcloud_msg_new, pointcloud_msg_old):
+                print(
+                    "Pointcloud number between old rosbag and new rosbag are different"
+                )
+                return False
+
+            # xyzの値が一緒であるべき
+            if not self._compare_xyz_in_pointcloud(
+                pointcloud_msg_new, pointcloud_msg_old
+            ):
+                print("XYZ value between old rosbag and new rosbag are different")
+                return False
+
+            # intensityの値が、tolerance = hogeで一緒であるべき
+            if not self._compare_intensity_in_pointcloud(
+                pointcloud_msg_new, pointcloud_msg_old, tolerance=0.1
+            ):
+                print("Intensity value between old rosbag and new rosbag are different")
+                return False
+
+        return True
+
+    def _check_dummy_field(self) -> bool:
+        # R,Cがdummyであるべき
+        for pointcloud_msg_new in self.rosbag_new.pointcloud_msgs[
+            "/sensing/lidar/concatenated/pointcloud"
+        ]:
+            pointcloud_array_new: np.ndarray = ros2_numpy.numpify(
+                pointcloud_msg_new.msg
+            )
+            if not np.allclose(pointcloud_array_new["return_type"], 0):
+                return False
+            if not np.allclose(pointcloud_array_new["channel"], 0):
+                return False
+
+        return True
+
+    def compare(self) -> bool:
+        if not self._compare_topic_infos():
+            return False
+
+        if not self._compare_pointcloud_data():
+            return False
+
+        if not self._check_dummy_field():
+            return False
+
+        return True
 
 
 def main(args):
@@ -167,33 +343,45 @@ def main(args):
         )
 
         for t4dataset_id in config["t4dataset_ids"]:
-            dataset_version, rosbag_name = webauto_t4dataset_interface.pull(
-                t4dataset_id
-            )
+            dataset_id, rosbag_name = webauto_t4dataset_interface.pull(t4dataset_id)
             print(f"download t4dataset to {work_dir_path}")
 
-            rosbag_path_old: Path = work_dir_path / dataset_version / "input_bag"
-            rosbag_path_new: Path = work_dir_path / dataset_version / f"{rosbag_name}"
+            rosbag_path_old: Path = work_dir_path / dataset_id / "input_bag"
+            rosbag_path_new: Path = work_dir_path / dataset_id / f"{rosbag_name}"
             process_bag(rosbag_path_old, rosbag_path_new)
             # 元のディレクトリを"input_bag"から"input_bag_old"に変更
             subprocess.run(
-                f"mv {rosbag_path_old} {work_dir_path / dataset_version / 'input_bag_old'}",
+                f"mv {rosbag_path_old} {work_dir_path / dataset_id / 'input_bag_old'}",
                 shell=True,
             )
             # 処理後のディレクトリを"input_bag"に変更
             subprocess.run(
-                f"mv {rosbag_path_new} {work_dir_path / dataset_version / 'input_bag'}",
+                f"mv {rosbag_path_new} {work_dir_path / dataset_id / 'input_bag'}",
                 shell=True,
             )
             # 2つのrosbagの内容を比較
-            compare(
-                t4dataset_dir_path=work_dir_path / dataset_version,
-                rosbag_name_old="input_bag_old",
-                rosbag_name_new="input_bag",
+            validator = ConvertedRosbagValidator(
+                rosbag_path_new=work_dir_path / dataset_id / "input_bag",
+                rosbag_path_old=work_dir_path / dataset_id / "input_bag_old",
             )
+            if validator.compare():
+                print("Validation result: OK")
+            else:
+                raise ValueError("Validation result: NG")
+
             # 比較結果が問題なければ、元のディレクトリを削除
+            subprocess.run(
+                f"rm -r {work_dir_path / dataset_id / 'input_bag_old'}",
+                shell=True,
+            )
             # t4datasetのディレクトリをWeb.Autoにアップロード
-            # webauto_t4dataset_interface.push(work_dir_path / dataset_version)
+            # webauto_t4dataset_interface.push(work_dir_path / dataset_id, t4dataset_id)
+
+            # アップロード後のディレクトリを削除
+            subprocess.run(
+                f"rm -r {work_dir_path / dataset_id}",
+                shell=True,
+            )
 
 
 if __name__ == "__main__":
